8696dcc2296bf24164a8d0d999b1bc898a85daf7
Joshuafl1 <joshuafl1@icloud.com>
2024-05-24 02:15:51 +0200

    Blog-App
    Homework for day 3 and 4

---
+++ Classwork/blog-app/front-end/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import HomePage from "./components/HomePage";
+
+export default function App() {
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
+}
---
+++ Classwork/blog-app/front-end/src/components/Heading/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./index.css";
+
+import "../BlogItem";
+
+export default function Heading() {
+  return <p className="page-title">THE BLOG</p>;
+}
