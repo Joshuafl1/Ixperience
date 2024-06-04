c38e6adc0e7e57c4b8dddc96cb3d63d2df4cfdcd
Joshuafl1 <joshuafl1@icloud.com>
2024-06-04 12:35:47 +0200

    Day 8

--- Classwork/blog-app/backend/routes/blogs.js
+++ Classwork/blog-app/backend/routes/blogs.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const blogController = require("../controllers/blogs");
+const blogController = require("../src/controllers/blogs");
 
 /**
  * POST /api/blogs
---
+++ Classwork/blog-app/backend/src/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const connectDB = require("./database/db");
+
+connectDB();
+
+const port = process.env.PORT || 8000;
+
+const blogsRoutes = require("../routes/blogs");
+
+const app = express();
+
+// Enabling CORS for any unknown origin
+app.use(cors());
+
+// Enabling the use of JSON for the body of the request
+app.use(express.json());
+
+app.use("/api/blogs", blogsRoutes);
+app.use("/api/categories", blogsRoutes);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
